3bb9214b1a433d02f1d5a9a0b738a9465aff24ff
motao123 <motao123@users.noreply.github.com>
2026-05-09 19:02:50 +0800

    fix: Vite add @ alias to resolve src directory

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { VitePWA } from 'vite-plugin-pwa'
+import { fileURLToPath } from 'url'
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
   plugins: [
     vue(),
     VitePWA({
